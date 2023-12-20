39984fbe143fee0ec059817f8ec03f5656160d59
Miguel Estêvão <miguelmariaestevao9@gmail.com>
2023-12-20 12:25:23 +0000

    Adicionei str

--- oi.py
+++ oi.py
@@ -1,18 +1,17 @@
 class Dog:
-    species = "Canis familoaris"
+    species = "Canis familiaris"
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     #Instance method
-    def description(self):
+    def __str__(self):
         return f"{self.name}, is {self.age} years old"
 
     def speak(self, sound):
         return f"{self.name} says {sound}"
 
 miles = Dog("Miles", 4)
-print(miles.description())
+print(miles)
 
-print(miles.speak("Woof Woof"))
