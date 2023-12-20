4ae62fa877c3cfeaaa26096e12426926c4251257
Miguel Estêvão <miguelmariaestevao9@gmail.com>
2023-12-20 12:03:31 +0000

    Implementei uma classe

--- oi.py
+++ oi.py
@@ -1,7 +1,18 @@
-print("Teste")
+class Dog:
+    species = "Canis familoaris"
 
-def soma(x,y):
-    adição = x + y 
-    return adição
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-print(soma(1,2))
+    #Instance method
+    def description(self):
+        return f"{self.name}, is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+miles = Dog("Miles", 4)
+print(miles.description())
+
+print(miles.speak("Woof Woof"))
